05dc85da38b0d5a6247da1f2cd508f70080df888
Weide <cbb110212@nptu.edu.tw>
2025-04-08 15:02:22 +0800

    side_bar close buttton

---
+++ docs/index.js
@@ -0,0 +1,18 @@
+const p = document.querySelector("#college_content");
+const button = document.querySelector("#show_college");
+
+button.addEventListener("click", readMore);
+
+function readMore() 
+{
+    if(p.className == "college_click")          // 顯示文字
+    {
+        p.className = "college_content";
+        button.innerHTML = "大學時期 &gt;";
+    } 
+    else    // 隱藏起文字
+    {
+        p.className = "college_click";
+        button.innerHTML = "大學時期 &lt;";
+    }
+}
